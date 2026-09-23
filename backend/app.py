3c78c09195aa78e4a9732cbe42b5df1fcccc928a
Fahad Ali <fahadali.dev66@gmail.com>
2026-09-23 08:20:15 +0500

    Initial muti-container-app

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Hello from Backend 🚀"
+
+app.run(host="0.0.0.0", port=5000)
+
